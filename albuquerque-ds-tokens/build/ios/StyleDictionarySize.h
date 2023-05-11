153d1b512365bc6f2b8c7cd5d9efb353d3b78fcd
brunoAlbuquerque1 <brunoalbuquerque.258@gmail.com>
2023-05-10 23:30:17 -0300

    chore: create project token

---
+++ albuquerque-ds-tokens/build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Thu, 11 May 2023 02:29:36 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
